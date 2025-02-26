54b01f5e091065229995f3bfcd549094e1d78ca6
ThomasVarvellaVicente <thomasvicente1234@gmail.com>
2025-02-26 12:30:05 -0500

    added controller layer files.

---
+++ api/src/main/java/com/webmaker/api/controllers/ProfileController.java
@@ -0,0 +1,9 @@
+package com.webmaker.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profiles")
+public class ProfileController {
+}
